46095c956af61fdeda873e4fff9005bb66a4732c
ZKAW <zkaw.exod@gmail.com>
2021-10-05 01:21:09 +0200

    Added comments

--- app.py
+++ app.py
@@ -13,14 +13,13 @@ def proxy_generator():
     
     return proxy
 
-def data_scraper(request_method, url, **kwargs):
+def data_scraper(request_method='get', url='https://icanhazip.com/', **kwargs): # Get page content through proxy server
     while True:
         try:
             proxy = proxy_generator()
             print("Proxy currently being used: {}".format(proxy))
             response = requests.request(request_method, url, proxies=proxy, timeout=5, **kwargs)
 
-            # if the request is successful, no exception is raised
         except KeyboardInterrupt:
             print('\nStopping...')
             exit()
@@ -39,7 +38,7 @@ def generate_list(length, max_ms=500, timeout=1, **kwargs):
 
     while valid < length:
         try:
-            proxy = proxy_generator()
+            proxy = proxy_generator() # Fetch a proxy address
 
             print("Proxy currently being used: {}".format(proxy))
             response = requests.request(request_method, url, proxies=proxy, timeout=timeout, **kwargs)
@@ -47,10 +46,10 @@ def generate_list(length, max_ms=500, timeout=1, **kwargs):
             ms = int(response.elapsed.total_seconds()*100)
             ms = round(ms, 2)
 
-            if ms > max_ms:
+            if ms > max_ms: # Set max response time to avoid laggy proxies
                 print(f'The response time was too high: {proxy} took {str(ms)}ms to response but {str(max_ms)}ms is required\n')
                 continue
-            else:
+            else: # Proxy is ready to work, push it in success list
                 proxyList.append(proxy)
                 print(f'New proxy: {proxy}\n-> {str(ms)}ms latency\n')
                 valid += 1
@@ -62,24 +61,20 @@ def generate_list(length, max_ms=500, timeout=1, **kwargs):
             else:
                 break
 
-        except:
+        except: # Could not connect to proxy server
             print("Connection error, looking for another proxy\n")
             pass
 
         print(f'Valid proxy: {str(valid)}/{str(length)}\n')
-    
+
+    # Save results into json file
     current_time = time.strftime("%Y-%m-%d-%H_%M")
     output_dir = f'proxyLists\\proxy_{current_time}.json'
-    
-    print(f'\nFinished processing proxy list. Results saved in {output_dir}\n')
     with open(output_dir, 'w') as f:
         json.dump(proxyList, f, sort_keys=True, indent=4)
+    
+    print(f'\nFinished processing proxy list. Results saved in {output_dir}\n')
+  
     return proxyList
-
-# response = data_scraper('get', "https://icanhazip.com/")
-# ms = str(response.elapsed.total_seconds()*100)
-
-# print(response.text)
-# print(f'response time: {ms}ms')
 
 print(generate_list(length=10, max_ms=150, timeout=0.3))
